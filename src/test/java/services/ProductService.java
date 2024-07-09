566337ac78eedef2272b02dcc569bd7d8ef33a61
kostikkv84 <kostikkv84@mail.ru>
2024-07-09 20:53:23 +0300

    Add new

--- src/test/java/services/ProductService.java
+++ src/test/java/services/ProductService.java
@@ -2,6 +2,7 @@ package services;
 
 import dto.ProductDTO;
 import entity.Products;
+import io.qameta.allure.Step;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -12,23 +13,23 @@ import java.util.List;
 public class ProductService {
 
     ProductDTO productDTO = new ProductDTO();
-
+    @Step("Находим продукт по Id")
     public Products findProduct(int id){
         return productDTO.findById(id);
     }
-
+    @Step("Сохраняем продукт")
     public void saveProduct(Products product){
         productDTO.save(product);
     }
-
+    @Step("Удаляем продукт")
     public void deleteProduct(Products product){
         productDTO.delete(product);
     }
-
+    @Step("Обновляем продукт")
     public void updateProduct(Products product){
         productDTO.update(product);
     }
-
+    @Step("ПОлучаем все продукты")
     public List<Products> finAllProduct(){
         return productDTO.findAll();
     }
